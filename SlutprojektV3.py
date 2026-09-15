b4dfdddae42fe53f1689e15078812a7a2d900bfc
Garfield <gustav.ohrqvist@gmail.com>
2026-09-15 12:38:58 +0200

    Patch.3
    Jag har implementerat ett API för att och plockar bort min lista jag använde som ett tillfälligt API. Jag har även vidare utvecklat mina olika klasser för att underlätta att hitta i koden.

--- SlutprojektV3.py
+++ SlutprojektV3.py
@@ -1,5 +1,5 @@
 #========================================
-#       AI JOBBMATCHNING V5
+#       AI JOBBMATCHNING V3
 #========================================
 # Mål: Matcha användarens profil mot jobbannonser
 # och visa vilka jobb som passar bäst.
